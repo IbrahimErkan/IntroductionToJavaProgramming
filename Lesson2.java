054f8e18e43b4b068ada46ed9d7d895d8e9caf4e
IbrahimErkan <76455945+IbrahimErkan@users.noreply.github.com>
2021-10-21 15:34:27 +0300

    Add files via upload

---
+++ Lesson2.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lesson2;
+
+/**
+ *
+ * @author erkan
+ */
+public class Lesson2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Variables:
+        
+        byte b; // 8 bit  -128-127
+        short s; // 16 bit
+        int i; // 32 bit
+        long l; // 64 bit
+        
+        float f; // 32 bit
+        double d; // 64 bit
+        
+        char c; // '&'
+        
+        boolean bool; // true - false
+        
+        
+        //Math Operators:
+        
+        // +,-,*,/,%
+        
+        
+        // Comments:
+        
+        // = Single Line
+        
+        /*
+        Multi Line(Block Comments)
+        */
+        
+        /**
+         * Documentation Comments
+         */
+        
+        //Sample Documentation
+        
+        /**
+         * @author İbrahim Erkan
+         * {@code} main(String[] args);
+         * @exception ArrayIndexOutOfBoundsException throws
+         * @param args an array of String objects 
+         * @return void
+         * @see Image
+         * @since 12.10.2021 12.56
+         * @version Lesson2 class v1
+         */
+        
+    }
+    
+}
