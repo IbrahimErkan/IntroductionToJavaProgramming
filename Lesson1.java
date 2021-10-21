2daa0d6dab9f0d610851f1bc7a9291b9c29af5d1
IbrahimErkan <76455945+IbrahimErkan@users.noreply.github.com>
2021-10-21 15:33:55 +0300

    Add files via upload

---
+++ Lesson1.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lesson1;
+
+/**
+ *
+ * @author erkan
+ */
+public class Lesson1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //first code
+        System.out.println("Hello World");
+        
+        //What is Java technology and why do I need it?
+        /*
+        Java is a programming language and computing platform first released by Sun Microsystems in 1995. 
+        There are lots of applications and websites that will not work unless you have Java installed, 
+        and more are created every day. Java is fast, secure, and reliable. 
+        From laptops to datacenters, game consoles to scientific supercomputers, 
+        cell phones to the Internet, Java is everywhere!
+        */
+        
+        //Is Java free to download?
+        /*
+        Yes, Java is free to download. Get the latest version at java.com.
+        you are building an embedded or consumer device and would like to include Java, 
+        please contact Oracle for more information on including Java in your device.
+        */
+        
+        //Why should I upgrade to the latest Java version?
+        /*
+        The latest Java version contains important enhancements to improve performance, 
+        stability and security of the Java applications that run on your machine. 
+        Installing this free update will ensure that your Java applications continue to run safely and efficiently.
+        */
+        
+        //MORE TECHNICAL INFORMATION
+        
+        //What will I get when I download Java software?
+        /*
+        The Java Runtime Environment (JRE) is what you get when you download Java software. 
+        The JRE consists of the Java Virtual Machine (JVM), Java platform core classes, 
+        and supporting Java platform libraries. The JRE is the runtime portion of Java software, 
+        which is all you need to run it in your Web browser.
+        */
+        
+        //What is Java Plug-in software?
+        /*
+        The Java Plug-in software is a component of the Java Runtime Environment (JRE). 
+        The JRE allows applets written in the Java programming language to run inside various browsers. 
+        The Java Plug-in software is not a standalone program and cannot be installed separately.
+        */
+        
+        //I have heard the terms Java Virtual Machine and JVM. Is this Java software?
+        /*
+        The Java Virtual Machine is only one aspect of Java software that is involved in web interaction. 
+        The Java Virtual Machine is built right into your Java software download,
+        and helps run Java applications.
+        */
+        
+        
+    }
+    
+}
