d8efd2a9d2c4a9814bbd89b09498be7334df5401
IbrahimErkan <76455945+IbrahimErkan@users.noreply.github.com>
2021-10-25 10:31:16 +0300

    Add files via upload

---
+++ Lesson9.java
@@ -0,0 +1,30 @@
+
+package lesson9;
+
+import java.util.Scanner;
+
+
+public class Lesson9 {
+
+    public static void main(String[] args) {
+        
+        //Classes and Objects
+        
+        /*
+        bir sınıfta üretilmiş nesneye : instance / örneklem
+        bir nesnenin özellikleri: 
+        kimliği(idendity) - Sınıf adı
+        özellikleri(attributes) - boy,yukseklik, yaş
+        davranışları(behaviars) - yürümek, konuşmak
+        */
+        
+        Car a = new Car();
+        a.startEngine();
+        
+        Car b = new Car();
+        b.startEngine();
+        
+        
+    }
+    
+}
