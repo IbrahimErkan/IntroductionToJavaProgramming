905f1a53045a5c9f6aebec06af91efe77c7b9f52
IbrahimErkan <76455945+IbrahimErkan@users.noreply.github.com>
2021-10-25 10:31:41 +0300

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lesson9;
+
+/**
+ *
+ * @author erkan
+ */
+class Car {
+    
+    int horsePower;
+    String color;
+    int numberOfDoor;
+    
+    public boolean startEngine(){
+        System.out.println("Start Engine!");
+        return true;
+    }
+}
