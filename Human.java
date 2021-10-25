b84e6ef93f2cb8b8a9145ce8896592b962aa999d
IbrahimErkan <76455945+IbrahimErkan@users.noreply.github.com>
2021-10-25 22:53:36 +0300

    Add files via upload

---
+++ Human.java
@@ -0,0 +1,42 @@
+package lesson12;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author erkan
+ */
+public class Human {
+
+    private String name;
+    private int age;
+
+    public Human(String name) {
+        this.name = name;
+        this.age = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) {
+            this.age = 0;
+        } else {
+            this.age = age;
+        }
+    }
+
+}
