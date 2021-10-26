73fb6d5e933c2772a87fe62f34bde3757d864b2d
IbrahimErkan <76455945+IbrahimErkan@users.noreply.github.com>
2021-10-26 11:31:22 +0300

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lesson15;
+
+/**
+ *
+ * @author erkan
+ */
+public class Dog extends Animal{
+    
+    @Override
+    public void noise(){
+        System.out.println("Hav Hav...");
+    }
+}
