027da981dc9e41ab1efe705a8f3f85ac582be6f1
IbrahimErkan <76455945+IbrahimErkan@users.noreply.github.com>
2021-10-26 10:57:25 +0300

    Add files via upload

---
+++ Car2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lesson14;
+
+/**
+ *
+ * @author erkan
+ */
+public class Car2 {
+    
+    int hp;
+    String brand;
+}
