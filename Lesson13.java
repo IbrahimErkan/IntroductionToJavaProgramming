b5de6cd5aa3e5fda2f51576ca2cfd4b9c8234716
IbrahimErkan <76455945+IbrahimErkan@users.noreply.github.com>
2021-10-26 08:47:02 +0300

    Add files via upload

---
+++ Lesson13.java
@@ -0,0 +1,20 @@
+package lesson13;
+
+public class Lesson13 {
+
+    public static void main(String[] args) {
+
+        //Math Class
+        
+        System.out.println(Math.abs(-15));
+        System.out.println(Math.ceil(2.1));
+        System.out.println(Math.floor(2.9));
+        System.out.println(Math.min(2,9));
+        System.out.println(Math.max(2,9));
+        System.out.println(Math.pow(3,3));
+        System.out.println(Math.sqrt(25));
+
+
+    }
+
+}
