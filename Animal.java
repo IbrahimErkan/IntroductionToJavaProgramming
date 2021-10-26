f67b7e06121678d0a7e9034a8ecaeff9cd54feec
IbrahimErkan <76455945+IbrahimErkan@users.noreply.github.com>
2021-10-26 11:30:14 +0300

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lesson15;
+
+/**
+ *
+ * @author erkan
+ */
+public class Animal {
+    public void noise(){
+        System.out.println("Grrr...");
+    }
+}
