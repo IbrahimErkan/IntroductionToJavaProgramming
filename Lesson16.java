8896044be5abd2767eba520ec0b2b168aab42ab0
IbrahimErkan <76455945+IbrahimErkan@users.noreply.github.com>
2021-10-27 20:31:33 +0300

    Add files via upload

---
+++ Lesson16.java
@@ -0,0 +1,40 @@
+
+package lesson16;
+
+public class Lesson16 {
+
+    public static void main(String[] args) {
+        
+        /**
+         Ağır Basma (Overriding) (Runtime Polymorphism)
+         * Aynı dönüş değeri ve argümanlar
+         * Erişim düzenleyicisi daha kısıtlayıcı olamaz
+         * Final ve static metodlar override edilemez
+         * Yapılandırıcılar override edilemez
+         */
+        
+        /**
+          Fazla Yükleme (Overloading)(Compile-Time Polymorphism)
+         */
+        
+        /**
+          Soyutlama (Abstraction)
+          * Abstract sınıflardan nesne oluşturulamaz
+          * Kalıtım ile kullanılmalıdır
+          * Abstract metod bulunduran sınıf abstract olmalıdır
+         */
+        
+        /**
+          Arayüzler (Interfaces)
+         * Sadece static final değişken bulundurabilir
+         * Yapılandırıcı bulunduramaz
+         * Başka arayüzleri kalıtım ile genişletebilir 
+         * Sınıf ve metodlar abstract olarak tanımlanmak zorunda değil
+         * Dolayl olarak zaten abstract (implicitly)
+         * Metodlar dolaylı olarak public
+         * Normal sınıflar gibi, sadece tek bir arayüzü genişletebilir ancak birçok arayüzü uygular
+         */
+        
+    }
+    
+}
